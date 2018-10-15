e82b634c9fae3b6676a812845d0d818f03da7fa5
dbrudner <dbrudne1@gmail.com>
2018-10-14 22:09:46 -0500

    removed comments from app/routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,24 +2,17 @@ const userRoutes = require("./user-routes");
 const keyboardRoutes = require("./keyboard-routes");
 
 module.exports = function(app, passport) {
-	// Gets all keyboards
 	keyboardRoutes.getAllKeyboards(app, "/api/keyboards/all");
 
-	// Post a new keyboard
 	keyboardRoutes.postKeyboard(app, "/api/new/keyboard");
 
-	// Get one keyboard
 	keyboardRoutes.getOneKeyboard(app, "/api/keyboard/:id");
 
-	// Checks if a user is logged in
 	userRoutes.test(app, "/api/test");
 
-	// Logs a user out
 	userRoutes.logout(app, "/api/logout");
 
-	// Logs a user in
 	userRoutes.login(app, passport, "/api/login");
 
-	// Signs up
 	userRoutes.signup(app, passport, "/api/signup");
 };
