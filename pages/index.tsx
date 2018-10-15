445387e26ba0aaa500a7660deb3fd04340781522
dbrudner <dbrudne1@gmail.com>
2018-10-14 22:09:29 -0500

    added login link to sign up page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Signup from "../client/signup";
+import Link from "next/link";
 
 const IndexPage = () => (
-	<div style={{ margin: "10%" }}>
+	<div style={{ maxWidth: "960px", margin: "auto" }}>
 		<h1>Sign up</h1>
 		<Signup />
+		<Link>
+			<a href="/login">Login</a>
+		</Link>
 	</div>
 );
 
